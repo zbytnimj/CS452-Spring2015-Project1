56ce9839729d7557c4bd00091d4e6e52b58e86bf
Janice T. Searleman <jetsza@gmail.com>
2015-02-24 12:10:27 -0500

    Create project1.js

---
+++ project1.js
@@ -0,0 +1,22 @@
+var gl;
+var points;
+window.onload = function init()
+{
+var canvas = document.getElementById( "gl-canvas" );
+gl = WebGLUtils.setupWebGL( canvas );
+if ( !gl ) { alert( "WebGL isn't available" ); }
+//
+// Configure WebGL
+//
+gl.viewport( 0, 0, canvas.width, canvas.height );
+gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+// Load shaders and initialize attribute buffers
+var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+gl.useProgram( program );
+
+render();
+};
+
+function render() {
+gl.clear( gl.COLOR_BUFFER_BIT );
+}
